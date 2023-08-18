0d3c72911941b66d95aa56b98b584b628a75d742
Jon Nordby <jononor@gmail.com>
2023-08-19 01:32:41 +0200

    digits: Use int16 data format, include GC

--- benchmarks/digits_run.py
+++ benchmarks/digits_run.py
@@ -59,7 +59,7 @@ def m2c_run(data):
 def none_run(data):
     errors = 0
     for idx, x in enumerate(data):
-        tmp = len(x)
+        #tmp = len(x)
         out = idx
         if (idx != out):
             errors += 1
@@ -67,30 +67,42 @@ def none_run(data):
 
 def benchmark():
 
-    data = digits
+    import gc
+
+    #data = digits
+    data = [ array.array('h', x) for x in digits ]
+
 
     print('model,errors,time_us')
 
+    gc.collect()
     before = time.ticks_us()
     none_errors = none_run(data)
+    gc.collect()
     after = time.ticks_us()
     none_duration = time.ticks_diff(after, before)
     print('none,{},{}'.format(none_errors, none_duration))
 
+    gc.collect()
     before = time.ticks_us()
     eml_errors = emlearn_run(data)
+    gc.collect()
     after = time.ticks_us()
     eml_duration = time.ticks_diff(after, before)
     print('emlearn,{},{}'.format(eml_errors, eml_duration))
 
+    gc.collect()
     before = time.ticks_us()
     everywhere_errors = everywhere_run(data)
+    gc.collect()
     after = time.ticks_us()
     everywhere_duration = time.ticks_diff(after, before)
     print('everywhere,{},{}'.format(everywhere_errors, everywhere_duration))
 
+    gc.collect()
     before = time.ticks_us()
     m2c_errors = m2c_run(data)
+    gc.collect()
     after = time.ticks_us()
     m2c_duration = time.ticks_diff(after, before)
     print('m2cgen,{},{}'.format(m2c_errors, m2c_duration))
