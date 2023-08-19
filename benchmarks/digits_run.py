67465610853b19304f17f0b3853071a9437b4289
Jon Nordby <jononor@gmail.com>
2023-08-19 03:03:13 +0200

    digits: Try prep for bigger model

--- benchmarks/digits_run.py
+++ benchmarks/digits_run.py
@@ -4,13 +4,9 @@ import array
 
 from digits_data import digits
 
-from everywhere_digits import RandomForestClassifier
-import m2c_digits
-import emltrees
 
-
-def emlearn_create():
-    model = emltrees.new(7, 150)
+def emlearn_create(emltrees):
+    model = emltrees.new(10, 3000)
 
     # Load a CSV file with the model
     with open('eml_digits.csv', 'r') as f:
@@ -27,8 +23,7 @@ def argmax(l):
     return max_idx
 
 
-clf = RandomForestClassifier()
-def everywhere_run(data):
+def everywhere_run(clf, data):
     errors = 0
     for idx, x in enumerate(data):
         #x = array.array('f', x)
@@ -37,8 +32,8 @@ def everywhere_run(data):
             errors += 1
     return errors
 
-model = emlearn_create()
-def emlearn_run(data):
+
+def emlearn_run(model, data):
     errors = 0
     for idx, x in enumerate(data):
         out = model.predict(x)
@@ -46,7 +41,7 @@ def emlearn_run(data):
             errors += 1
     return errors
 
-def m2c_run(data):
+def m2c_run(m2c_digits, data):
     errors = 0
     for idx, x in enumerate(data):
         #x = array.array('f', x)
@@ -83,28 +78,37 @@ def benchmark():
     none_duration = time.ticks_diff(after, before)
     print('none,{},{}'.format(none_errors, none_duration))
 
+    import emltrees
+    model = emlearn_create(emltrees)
     gc.collect()
     before = time.ticks_us()
-    eml_errors = emlearn_run(data)
+    eml_errors = emlearn_run(model, data)
     gc.collect()
     after = time.ticks_us()
     eml_duration = time.ticks_diff(after, before)
     print('emlearn,{},{}'.format(eml_errors, eml_duration))
+    del model
+    del emltrees
 
+    import everywhere_digits
+    clf = everywhere_digits.RandomForestClassifier()  
     gc.collect()
     before = time.ticks_us()
-    everywhere_errors = everywhere_run(data)
+    everywhere_errors = everywhere_run(clf, data)
     gc.collect()
     after = time.ticks_us()
     everywhere_duration = time.ticks_diff(after, before)
     print('everywhere,{},{}'.format(everywhere_errors, everywhere_duration))
+    del clf
+    del everywhere_digits
 
-    gc.collect()
-    before = time.ticks_us()
-    m2c_errors = m2c_run(data)
-    gc.collect()
-    after = time.ticks_us()
-    m2c_duration = time.ticks_diff(after, before)
-    print('m2cgen,{},{}'.format(m2c_errors, m2c_duration))
-
+    #import m2c_digits
+    #gc.collect()
+    #before = time.ticks_us()
+    #m2c_errors = m2c_run(m2c_digits, data)
+    #gc.collect()
+    #after = time.ticks_us()
+    #m2c_duration = time.ticks_diff(after, before)
+    #print('m2cgen,{},{}'.format(m2c_errors, m2c_duration))
+    #del m2c_digits
 
